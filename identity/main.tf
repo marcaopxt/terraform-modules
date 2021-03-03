0f3a290a780a822151af613ef1d08f86b3575867
Marcus Peixoto <marcuspxt@gmail.com>
2021-03-03 12:06:02 -0300

    feat(Keycloak): Missing module

--- identity/main.tf
+++ identity/main.tf
@@ -1,14 +1,25 @@
+data "template_file" "keycloak_release_template" {
+  template = file("${path.module}/templates/keycloak-values.tpl.yaml")
+  vars     = var.keycloak_release_config
+}
 resource "kubernetes_namespace" "keycloak" {
+  count         = var.keycloak_enabled == false ? 0 : 1
   metadata {
     annotations = {}
     labels = {}
     name = "keycloak"
   }
 }
-resource "kubernetes_namespace" "authentication" {
-  metadata {
-    annotations = {}
-    labels = {}
-    name = "authentication"
-  }
+resource "helm_release" "keycloak" {
+  count         = var.keycloak_enabled == false ? 0 : 1
+  name          = "keycloak"
+  namespace     = "keycloak" 
+  chart         = "keycloak"
+  repository    = "https://codecentric.github.io/helm-charts"
+  version       = "9.9.5"
+  reuse_values  = false
+  recreate_pods = true
+  force_update  = true
+
+  values        = [ data.template_file.keycloak_release_template.rendered ]
 }
---
+++ identity/variables.tf
@@ -0,0 +1,9 @@
+variable keycloak_enabled {
+  type = bool
+  default = true
+}
+variable "keycloak_release_config" {
+  type    = map
+  default = {
+  }
+}
